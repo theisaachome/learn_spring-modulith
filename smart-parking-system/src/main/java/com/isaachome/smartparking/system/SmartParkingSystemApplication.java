31d1bb6bedc1c330c5279f5dd51a016d115257b5
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-10-19 15:56:17 +0800

    1st commit

---
+++ smart-parking-system/src/main/java/com/isaachome/smartparking/system/SmartParkingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.isaachome.smartparking.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkingSystemApplication.class, args);
+    }
+
+}
---
+++ smart-parking-system/src/test/java/com/isaachome/smartparking/system/SmartParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaachome.smartparking.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
